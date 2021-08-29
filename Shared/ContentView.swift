85996556c8696a0e0079e866e66145d3c289cf9c
gnehs <jayuiop@gmail.com>
2021-08-29 09:52:02 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 勝勝寶寶 on 2021/8/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/PokaNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokaNativeApp.swift
+//  Shared
+//
+//  Created by 勝勝寶寶 on 2021/8/29.
+//
+
+import SwiftUI
+
+@main
+struct PokaNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
