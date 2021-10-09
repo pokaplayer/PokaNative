c06b0f441f1ff39f7c22a35ab8cb46d0d145a31b
gnehs <jayuiop@gmail.com>
2021-10-09 22:10:04 +0800

    updated playlistview

--- Shared/Views/PlaylistsView.swift
+++ Shared/Views/PlaylistsView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 struct PlaylistCoverView: View {
     var coverURL: String
-    var baseURL = defaults.string(forKey: "baseURL") ?? "" 
+    var baseURL = defaults.string(forKey: "baseURL") ?? ""
     var body: some View {
         AsyncImage(url: URL(string: coverURL.hasPrefix("http") ? coverURL : baseURL + coverURL)){ image in
             image
                 .resizable()
-                .aspectRatio(1, contentMode: .fill)
+                .scaledToFill()
         } placeholder: {
             ProgressView()
         }
@@ -21,48 +21,83 @@ struct PlaylistCoverView: View {
         .cornerRadius(5)
     }
 }
+
+struct PlaylistItemView: View {
+    var playlist: Playlist
+    
+    var body: some View {
+        NavigationLink(destination: FolderView()) {
+            HStack{
+                if playlist.image != nil {
+                    PlaylistCoverView(coverURL: playlist.image ?? "/img/icons/apple-touch-icon.png")
+                } else {
+                    Image(systemName: "music.note.list")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .frame(width: 40, height: 40)
+                        .cornerRadius(5)
+                }
+                Text(playlist.name)
+            }
+        }
+    }
+}
+
 struct PlaylistsView: View {
     
     var title: String?
-    var playlist: Playlist?
+    var playlist: [Playlist]?
     @State var resData: PlaylistReponse?
     var body: some View {
         
         VStack{
             List{
-                Section(header: Text("Playlists")) {
-                    ForEach(resData?.playlists ?? [Playlist](), id: \.self) { item in
-                        NavigationLink(destination: FolderView()) {
-                            HStack{
-                                PlaylistCoverView(coverURL: item.image ?? "/img/icons/apple-touch-icon.png")
-                                Text(item.name)
+                if resData != nil {
+                    Section(header: Text("Playlists")) {
+                        ForEach(resData?.playlists ?? [Playlist](), id: \.self) { item in
+                            PlaylistItemView(playlist: item)
+                        }
+                    }
+                    Section(header: Text("Folders")) {
+                        ForEach(resData?.playlistFolders ?? [PlaylistFolder](), id: \.self) { item in
+                            NavigationLink(destination: PlaylistsView(title: item.name, playlist: item.playlists)) {
+                                HStack{
+                                    if item.image != nil {
+                                        PlaylistCoverView(coverURL: item.image ?? "/img/icons/apple-touch-icon.png")
+                                    } else {
+                                        Image(systemName: "folder")
+                                            .padding()
+                                            .foregroundColor(.white)
+                                            .background(Color.green)
+                                            .frame(width: 40, height: 40)
+                                            .cornerRadius(5)
+                                    }
+                                    Text(item.name)
+                                }
                             }
                         }
                     }
-                }
-                Section(header: Text("Folders")) {
-                    ForEach(resData?.playlistFolders ?? [PlaylistFolder](), id: \.self) { item in
-                        NavigationLink(destination: FolderView()) {
-                            HStack{
-                                PlaylistCoverView(coverURL: item.image ?? "/img/icons/apple-touch-icon.png")
-                                Text(item.name)
-                            }
-                        }
+                } else {
+                    ForEach(playlist ?? [Playlist](), id: \.self) { item in
+                        PlaylistItemView(playlist: item)
                     }
                 }
             }
         }.onAppear() {
-            PokaAPI.shared.getPlaylists() { (result) in
-                self.resData = result
+            if playlist == nil {
+                PokaAPI.shared.getPlaylists() { (result) in
+                    self.resData = result
+                }
             }
         }
         .navigationTitle(title ?? "Playlists")
     }
 }
 /*
-struct PlaylistsView_Previews: PreviewProvider {
-    static var previews: some View {
-        PlaylistsView()
-    }
-}
-*/
+ struct PlaylistsView_Previews: PreviewProvider {
+ static var previews: some View {
+ PlaylistsView()
+ }
+ }
+ */
