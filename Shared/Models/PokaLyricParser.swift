683159b48d649cd9a1a83baece70e498d32f94fa
gnehs <jayuiop@gmail.com>
2021-11-30 21:16:07 +0800

    trim lyric
    Signed-off-by: gnehs <jayuiop@gmail.com>

--- Shared/Models/PokaLyricParser.swift
+++ Shared/Models/PokaLyricParser.swift
@@ -26,7 +26,7 @@ class PokaLyricParser: ObservableObject {
         let lines = lyric
             .replacingOccurrences(of: "\\n", with: "\n")
             .components(separatedBy: .newlines)
-
+        
         for line in lines {
             parseLyric(line: line)
         }
@@ -38,7 +38,7 @@ class PokaLyricParser: ObservableObject {
             }
         }
     }
-
+    
     private func parseHeader(prefix: String, line: String) -> String? {
         if line.hasPrefix("[" + prefix + ":"), line.hasSuffix("]") {
             let startIndex = line.index(line.startIndex, offsetBy: prefix.count + 2)
@@ -48,7 +48,7 @@ class PokaLyricParser: ObservableObject {
             return nil
         }
     }
-
+    
     func timeStampToTime(timeStamp: String) -> Double {
         let str: String = timeStamp
             .replacingOccurrences(of: "[", with: "")
@@ -59,20 +59,20 @@ class PokaLyricParser: ObservableObject {
         let res: Double = min + sec
         return res
     }
-
+    
     private func parseLyric(line: String) {
         let chomp: String = line.replacingOccurrences(of: "\r", with: "")
         let regex = try! NSRegularExpression(pattern: "\\[[0-9]{1,}\\:[0-9]{1,2}(\\.[0-9]{1,})?\\]", options: NSRegularExpression.Options.caseInsensitive)
         let matches = regex.matches(in: chomp, options: NSRegularExpression.MatchingOptions.reportProgress, range: NSMakeRange(0, chomp.count))
-
+        
         if matches.count > 0 {
             // find the lyrics str first
             let last = matches.last! as NSTextCheckingResult
-
-            let text = (chomp as NSString).substring(
-                with: NSMakeRange(last.range.location + last.range.length, chomp.count - (last.range.location + last.range.length))
-            )
-
+            
+            let text = (chomp as NSString)
+                .substring(with: NSMakeRange(last.range.location + last.range.length, chomp.count - (last.range.location + last.range.length)))
+                .trimmingCharacters(in: .whitespaces)
+            
             for match in matches {
                 let temp = (chomp as NSString).substring(with: match.range)
                 let time = timeStampToTime(timeStamp: temp)
@@ -80,7 +80,7 @@ class PokaLyricParser: ObservableObject {
             }
         }
     }
-
+    
     func getCurrentLineIndex(time: Double) -> Int {
         var res = 0
         for (index, item) in lyricItems.enumerated() {
