55ceca6766a48fe77da6e9ee8bb977d1fa0a2907
gnehs <jayuiop@gmail.com>
2022-10-16 17:19:01 +0800

    added player swipe song action
    Signed-off-by: gnehs <jayuiop@gmail.com>

--- Shared/Views/Components/Player/PlayerView.swift
+++ Shared/Views/Components/Player/PlayerView.swift
@@ -45,6 +45,32 @@ struct PlayerTimeView: View {
     }
 }
 
+struct PlayerCoverView: View {
+    var coverURL: String
+    var coverSize: CGFloat
+    var body: some View {
+        CachedAsyncImage(url: URL(string: PokaURLParser(coverURL))) { image in
+            image.resizable()
+                .scaledToFill()
+                .frame(width: coverSize, height: coverSize)
+                .clipped()
+                .cornerRadius(8)
+        } placeholder: {
+            ZStack {
+                Rectangle()
+                    .fill(.white.opacity(0.1))
+                    .frame(width: coverSize, height: coverSize)
+                    .cornerRadius(8)
+                ProgressView()
+            }
+        }
+        .frame(width: UIScreen.main.bounds.size.width - 40, alignment: .center)
+        .aspectRatio(1, contentMode: .fit)
+        .padding(20.0)
+        .shadow(color: Color.black.opacity(0.2), radius: 10.0, y: 10.0)
+    }
+}
+
 struct DurationTimeView: View {
     var body: some View {
         Text(formatTime(seconds: player.currentItem?.asset.duration.seconds ?? 0))
@@ -62,108 +88,126 @@ func formatTime(seconds: TimeInterval) -> String {
 
 struct PlayerView: View {
     @StateObject private var ppplayer = player
-    @State var coverSize: CGFloat = min(460, UIScreen.main.bounds.width - 40)
+    @GestureState private var translation: CGFloat = 0
+    @State private var coverSize: CGFloat = min(460, UIScreen.main.bounds.width - 40)
     var body: some View {
         if ppplayer.currentPlayingItem != nil {
             VStack {
-                VStack {
-                    Spacer()
-                    CachedAsyncImage(url: URL(string: PokaURLParser(player.currentPlayingItem!.song.cover))) { image in
-                        image.resizable()
-                            .scaledToFill()
-                            .frame(width: coverSize, height: coverSize)
-                            .clipped()
-                            .cornerRadius(8)
-                    } placeholder: {
-                        ZStack {
-                            Rectangle()
-                                .fill(.white.opacity(0.1))
-                                .aspectRatio(1.0, contentMode: .fit)
-                                .cornerRadius(8)
-                            ProgressView()
-                        }
+                Spacer()
+                GeometryReader { geometry in
+                    LazyHStack(spacing: 0) {
+                        PlayerCoverView(coverURL: player.previousPlayItem!.song.cover, coverSize: coverSize)
+                            .frame(width: UIDevice.isIPhone ? UIScreen.main.bounds.size.width - 40 : 500.0)
+                            .opacity(min(translation / geometry.size.width, 0.95))
+                            .scaleEffect(min(1, translation / geometry.size.width + 0.5), anchor: .center)
+                        PlayerCoverView(coverURL: player.currentPlayingItem!.song.cover, coverSize: coverSize)
+                            .frame(width: UIDevice.isIPhone ? UIScreen.main.bounds.size.width - 40 : 500.0)
+                            .scaleEffect(translation == 0 ? 1 : max(((80 - abs(translation)) / 80) * 0.2 + 0.8, 0.8), anchor: .center)
+                            .opacity(translation == 0 ? 1 : 0.8)
+                        PlayerCoverView(coverURL: player.nextPlayItem!.song.cover, coverSize: coverSize)
+                            .frame(width: UIDevice.isIPhone ? UIScreen.main.bounds.size.width - 40 : 500.0)
+                            .opacity(min(-translation / geometry.size.width, 0.95))
+                            .scaleEffect(min(1, -translation / geometry.size.width + 0.5), anchor: .center)
                     }
-                    .frame(width: UIScreen.main.bounds.size.width - 40, alignment: .center)
-                    .aspectRatio(1, contentMode: .fit)
-                    .padding(20.0)
-                    .shadow(color: Color.black.opacity(0.2), radius: 10.0, y: 10.0)
-
-                    VStack(alignment: .leading) {
-                        Text(player.currentPlayingItem!.song.name)
-                            .font(/*@START_MENU_TOKEN@*/ .headline/*@END_MENU_TOKEN@*/)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color.white)
-                        Text(player.currentPlayingItem!.song.artist)
-                            .font(.subheadline)
-                            .foregroundColor(Color.white)
-                            .opacity(0.75)
-                        HStack {
-                            Spacer()
-                        }
-                    }.padding(.horizontal, 20)
-                    PlayerTimeView()
-                    HStack {
-                        Spacer()
-
-                        Button(action: { player.shuffle() }) {
-                            Image(systemName: "dice")
-                                .font(.system(size: 20))
-                                .padding()
-                                .foregroundColor(Color.white)
-                        }
-                        .buttonStyle(PlainButtonStyle())
-                        .hoverEffect()
-
-                        Button(action: { player.previousTrack() }) {
-                            Image(systemName: "backward.end.alt")
-                                .font(.system(size: 20))
-                                .padding()
-                                .foregroundColor(Color.white)
-                        }
-                        .buttonStyle(PlainButtonStyle())
-                        .hoverEffect()
-
-                        Button(action: { ppplayer.isPaused ? player.playTrack() : player.pause() }) {
-                            if player.isLoading {
-                                ProgressView()
-                                    .font(/*@START_MENU_TOKEN@*/ .title3/*@END_MENU_TOKEN@*/)
-                                    .frame(width: 24, height: 24)
-                            } else {
-                                Image(systemName: ppplayer.isPaused ? "play" : "pause")
-                                    .font(.system(size: 24))
-                                    .padding()
-                                    .foregroundColor(Color.white)
+                    .offset(x: -geometry.size.width + (UIDevice.isIPhone ? 40 * 1.5 : 0))
+                    .offset(x: translation)
+                    .gesture(
+                        DragGesture().updating($translation) { value, state, _ in
+                            if abs(value.translation.width) > 10 {
+                                withAnimation(.interactiveSpring()) {
+                                    state = value.translation.width
+                                }
+                            }
+                        }.onEnded { value in
+                            if abs(value.translation.width) > (coverSize / 2) {
+                                let impact = UIImpactFeedbackGenerator(style: .medium)
+                                impact.impactOccurred()
+                                if value.translation.width < 0 {
+                                    ppplayer.nextTrack()
+                                } else {
+                                    ppplayer.previousTrack()
+                                }
                             }
                         }
-                        .buttonStyle(PlainButtonStyle())
-                        .frame(width: 56, height: 56)
-                        .background(Color.white.opacity(0.1))
-                        .cornerRadius(.infinity)
-                        .contentShape(RoundedRectangle(cornerRadius: 500, style: .continuous))
-                        .hoverEffect()
+                    )
+                }
+                .frame(height: coverSize)
 
-                        Button(action: { player.nextTrack() }) {
-                            Image(systemName: "forward.end.alt")
-                                .font(.system(size: 20))
-                                .padding()
-                                .foregroundColor(Color.white)
-                        }
-                        .buttonStyle(PlainButtonStyle())
-                        .hoverEffect()
-
-                        Button(action: {}) {
-                            Image(systemName: "forward.end.alt")
-                                .font(.system(size: 20))
-                                .padding()
-                                .foregroundColor(Color.white)
-                        }
-                        .buttonStyle(PlainButtonStyle())
-                        .opacity(0)
-
+                VStack(alignment: .leading) {
+                    Text(player.currentPlayingItem!.song.name)
+                        .font(/*@START_MENU_TOKEN@*/ .headline/*@END_MENU_TOKEN@*/)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                    Text(player.currentPlayingItem!.song.artist)
+                        .font(.subheadline)
+                        .foregroundColor(Color.white)
+                        .opacity(0.75)
+                    HStack {
                         Spacer()
                     }
+                }.padding(.horizontal, 20).padding(.top, 32)
+                PlayerTimeView()
+                HStack {
+                    Spacer()
+
+                    Button(action: { player.shuffle() }) {
+                        Image(systemName: "dice")
+                            .font(.system(size: 20))
+                            .padding()
+                            .foregroundColor(Color.white)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .hoverEffect()
+
+                    Button(action: { player.previousTrack() }) {
+                        Image(systemName: "backward.end.alt")
+                            .font(.system(size: 20))
+                            .padding()
+                            .foregroundColor(Color.white)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .hoverEffect()
+
+                    Button(action: { ppplayer.isPaused ? player.playTrack() : player.pause() }) {
+                        if player.isLoading {
+                            ProgressView()
+                                .font(/*@START_MENU_TOKEN@*/ .title3/*@END_MENU_TOKEN@*/)
+                                .frame(width: 24, height: 24)
+                        } else {
+                            Image(systemName: ppplayer.isPaused ? "play" : "pause")
+                                .font(.system(size: 24))
+                                .padding()
+                                .foregroundColor(Color.white)
+                        }
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .frame(width: 56, height: 56)
+                    .background(Color.white.opacity(0.1))
+                    .cornerRadius(.infinity)
+                    .contentShape(RoundedRectangle(cornerRadius: 500, style: .continuous))
+                    .hoverEffect()
+
+                    Button(action: { player.nextTrack() }) {
+                        Image(systemName: "forward.end.alt")
+                            .font(.system(size: 20))
+                            .padding()
+                            .foregroundColor(Color.white)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .hoverEffect()
+
+                    Button(action: {}) {
+                        Image(systemName: "forward.end.alt")
+                            .font(.system(size: 20))
+                            .padding()
+                            .foregroundColor(Color.white)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .opacity(0)
+
                     Spacer()
                 }
+                Spacer()
             }
         } else {
             Text("No songs in queue")
